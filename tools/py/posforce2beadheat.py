7549b9a4d271f35eb20453ed9f7c9aa7ee1715a6
Mariana Rossi <mariana.rossi@chem.ox.ac.uk>
2018-12-06 15:58:09 +0100

    Something more complete

--- tools/py/posforce2beadheat.py
+++ tools/py/posforce2beadheat.py
@@ -5,7 +5,7 @@
 Reads positions and forces from an i-PI run and computes the bead heat component.
 
 Assumes the input files are in xyz format and atomic units, with
-prefix.pos_*.xyz and prefix.for_*.xyz naming scheme.
+prefix.pos_*.xyz and prefix.mom_*.xyz naming scheme.
 
 Syntax:
    posforce2beadheat.py prefix temperature[K]
@@ -27,15 +27,15 @@ def main(prefix, temp):
 
     T = float(temp)
     fns_pos = sorted(glob.glob(prefix + ".pos*"))
-    fns_for = sorted(glob.glob(prefix + ".for*"))
-    fn_out_kin = prefix + ".heat.xyz"
+    fns_for = sorted(glob.glob(prefix + ".mom*"))
+    fn_out_heat = prefix + ".heat.xyz"
 
     # check that we found the same number of positions and forces files
     nbeads = len(fns_pos)
-    if nbeads != len(fns_for):
+    if nbeads != len(fns_mom):
         print fns_pos
-        print fns_for
-        raise ValueError("Mismatch between number of input files for forces and positions.")
+        print fns_mom
+        raise ValueError("Mismatch between number of input files for momenta and positions.")
 
     # print some information
     print 'temperature = {:f} K'.format(T)
@@ -47,17 +47,19 @@ def main(prefix, temp):
         print '{:s}   {:s}'.format(fn_pos, fn_for)
     print
     print 'output file names:'
-    print fn_out_kin
-    print fn_out_kod
+    print fn_out_heat
     print
 
     temp = unit_to_internal("energy", "kelvin", T)
 
+    # Calculate w_P, which just depends on the temperature and nbeads
+
+    wp=temp/nbeads
+
     # open input and output files
     ipos = [open(fn, "r") for fn in fns_pos]
-    ifor = [open(fn, "r") for fn in fns_for]
-    ikin = open(fn_out_kin, "w")
-    ikod = open(fn_out_kod, "w")
+    imom = [open(fn, "r") for fn in fns_mom]
+    iheat = open(fn_out_heat, "w")
 
     natoms = 0
     ifr = 0
@@ -73,50 +75,51 @@ def main(prefix, temp):
             for i in range(nbeads):
                 ret = read_file("xyz", ipos[i])
                 pos = ret["atoms"]
-                ret = read_file("xyz", ifor[i])
-                force = ret["atoms"]
+                ret = read_file("xyz", imom[i])
+                mom = ret["atoms"]
                 if natoms == 0:
                     natoms = pos.natoms
                     beads = Beads(natoms, nbeads)
-                    forces = Beads(natoms, nbeads)
-                    kcv = np.zeros((natoms, 6), float)
+                    moms = Beads(natoms, nbeads)
+                    heat = np.zeros((natoms, 3), float)
                 beads[i].q = pos.q
-                forces[i].q = force.q
+                moms[i].q = mom.q
         except EOFError:
             # finished reading files
             break
 
         # calculate kinetic energies
         q = dstrip(beads.q)
-        f = dstrip(forces.q)
-        qc = dstrip(beads.qc)
-        kcv[:] = 0
+        p = dstrip(moms.q)
+        heat[:] = 0.0
         for j in range(nbeads):
             for i in range(natoms):
-                kcv[i, 0] += (q[j, i * 3 + 0] - qc[i * 3 + 0]) * f[j, i * 3 + 0]
-                kcv[i, 1] += (q[j, i * 3 + 1] - qc[i * 3 + 1]) * f[j, i * 3 + 1]
-                kcv[i, 2] += (q[j, i * 3 + 2] - qc[i * 3 + 2]) * f[j, i * 3 + 2]
-                kcv[i, 3] += (q[j, i * 3 + 0] - qc[i * 3 + 0]) * f[j, i * 3 + 1] + (q[j, i * 3 + 1] - qc[i * 3 + 1]) * f[j, i * 3 + 0]
-                kcv[i, 4] += (q[j, i * 3 + 0] - qc[i * 3 + 0]) * f[j, i * 3 + 2] + (q[j, i * 3 + 2] - qc[i * 3 + 2]) * f[j, i * 3 + 0]
-                kcv[i, 5] += (q[j, i * 3 + 1] - qc[i * 3 + 1]) * f[j, i * 3 + 2] + (q[j, i * 3 + 2] - qc[i * 3 + 2]) * f[j, i * 3 + 1]
-        kcv *= -0.5 / nbeads
-        kcv[:, 0:3] += 0.5 * Constants.kb * temp
-        kcv[:, 3:6] *= 0.5
-
+                if j==0:
+                   jminus=nbeads-1
+                   jplus=j+1
+                if j==(nbeads-1):
+                   jminus=j-1
+                   jplus=0
+                else:
+                   jplus=j+1
+                   jminus=j-1
+                heat[i, 0] += ((q[jplus, i * 3 + 0] - q[j, i * 3 + 0]) * (-q[jplus, i * 3 + 0] + q[j, i * 3 + 0]) * p[j, i * 3 + 0] + (q[jminus, i * 3 + 0] - q[j, i * 3 + 0]) * (-q[jminus, i * 3 + 0] + q[j, i * 3 + 0]) * p[j, i * 3 + 0]  )
+                heat[i, 1] += ((q[jplus, i * 3 + 1] - q[j, i * 3 + 1]) * (-q[jplus, i * 3 + 1] + q[j, i * 3 + 1]) * p[j, i * 3 + 1] + (q[jminus, i * 3 + 1] - q[j, i * 3 + 1]) * (-q[jminus, i * 3 + 1] + q[j, i * 3 + 1]) * p[j, i * 3 + 1]  )
+                heat[i, 2] += ((q[jplus, i * 3 + 0] - q[j, i * 3 + 2]) * (-q[jplus, i * 3 + 2] + q[j, i * 3 + 2]) * p[j, i * 3 + 2] + (q[jminus, i * 3 + 2] - q[j, i * 3 + 2]) * (-q[jminus, i * 3 + 2] + q[j, i * 3 + 2]) * p[j, i * 3 + 2]  )
+        heat *= wp**2 / nbeads
+#        kcv[:, 0:3] += 0.5 * Constants.kb * temp
+#        kcv[:, 3:6] *= 0.5
+        fullheat = np.sum(heat, axis=0)
         # write output
-        ikin.write("%d\n# Centroid-virial kinetic energy estimator [a.u.] - diagonal terms: xx yy zz\n" % natoms)
-        ikod.write("%d\n# Centroid-virial kinetic energy estimator [a.u.] - off-diag terms: xy xz yz\n" % natoms)
+        iheat.write("%d\n# Full bead heat flux [a.u.] (x, y, z) = %.8f %.8f %.8f\n" % (natoms, fullheat[0], fullheat[1], fullheat[2])
         for i in range(natoms):
-            ikin.write("%8s %12.5e %12.5e %12.5e\n" % (pos.names[i], kcv[i, 0], kcv[i, 1], kcv[i, 2]))
-            ikod.write("%8s %12.5e %12.5e %12.5e\n" % (pos.names[i], kcv[i, 3], kcv[i, 4], kcv[i, 5]))
+            iheat.write("%8s %12.5e %12.5e %12.5e\n" % (pos.names[i], heat[i, 0], heat[i, 1], heat[i, 2]))
 
         ifr += 1
 
     print '\rProcessed {:d} frames.'.format(ifr)
 
     ikin.close()
-    ikod.close()
-
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
